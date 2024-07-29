0e72d0112b66b63472c2e38b9e15abff8c4bc001
Tau <tausifsolaiman@gmail.com>
2024-07-29 09:52:30 -0400

    react router dom routing

---
+++ src/Post.js
@@ -0,0 +1,17 @@
+export default function Post () {
+    return (
+        <div className="post">
+            <div className="image">
+            <img src="https://techcrunch.com/wp-content/uploads/2024/07/Screenshot-2024-07-25-at-1.57.32%E2%80%AFPM.jpg?resize=2048,1356"></img>
+            </div>
+            <div className="texts">
+            <h2>A tale of two foldables</h2>
+            <p className="info">
+                <a className="author">Tau Solaiman</a>
+                <time>2024-6-28 16:45</time>
+            </p>
+            <p className="summary">Samsung’s original Galaxy Flip was the phone that taught me to love foldables. Prior to the launch of the clamshell, devices were big and bulky like the Galaxy Fold. Rather than embracing portability, the goal was jamming as much screen real estate as possible into a pocketable design. The result, in the case of the Fold, was massive, even when closed.</p>
+            </div>  
+      </div>
+    )
+}
