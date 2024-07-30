9a67e12208b5d63b647f948d0be931a4c295f2c0
Tau <tausifsolaiman@gmail.com>
2024-07-29 20:05:39 -0400

    authentication with jwt

---
+++ api/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express(); 
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const UserModel = require('./models/User');
+const bcrypt = require('bcryptjs'); 
+const jwt = require('jsonwebtoken');
+
+const salt = bcrypt.genSaltSync(10);
+const secret = 'asdfe45we45w345wegw345werjktjwertkj';
+
+app.use(cors({credentials: true, origin: 'http://localhost:3000'})); 
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://blog:kTSynLJisDsMpagb@cluster0.mnvwtnr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    try {
+        const userDoc = await UserModel.create({
+            username,
+            password: bcrypt.hashSync(password, salt),
+        })
+        res.json(userDoc); 
+    } catch (e) {
+        console.log(e); 
+        res.status(400).json(e); 
+    }
+})
+
+app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await UserModel.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      // logged in
+      jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json({
+          id:userDoc._id,
+          username,
+        });
+      });
+    } else {
+      res.status(400).json('wrong credentials');
+    }
+  });
+
+
+app.listen(4000);
+
