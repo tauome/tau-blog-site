fe4d8926fc962e139e4c0c7aa077e639fb4e187e
Tau <tausifsolaiman@gmail.com>
2024-07-30 15:42:57 -0400

    Create new post page

--- api/index.js
+++ api/index.js
@@ -3,9 +3,13 @@ const app = express();
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 const UserModel = require('./models/User');
+const PostModel = require('./models/Post'); 
 const bcrypt = require('bcryptjs'); 
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser'); 
+const multer = require('multer'); 
+const uploadMiddleware = multer({dest: 'uploads/'});
+const fs = require('fs'); 
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
@@ -59,6 +63,23 @@ app.post('/login', async (req,res) => {
   app.post('/logout', (req, res) => {
     res.cookie('token', '').json('ok');
   })
+
+  app.post('/post',uploadMiddleware.single('file'), async (req,res) => {
+    const {originalname, path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1]; 
+    const newPath = path +'.'+ext;
+    fs.renameSync(path, newPath )
+
+    const {title, summary, content} = req.body; 
+    const postDoc = await PostModel.create({
+        title,
+        summary,
+        content,
+        cover: newPath
+    })
+    res.json(postDoc); 
+  });
 
 
 app.listen(4000);
