d6545fb8e1783ca71c99151a72602e580c0cb8b3
Tau <tausifsolaiman@gmail.com>
2024-07-31 15:06:30 -0400

    edit post functionality

--- api/index.js
+++ api/index.js
@@ -21,6 +21,7 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 
 mongoose.connect('mongodb+srv://blog:kTSynLJisDsMpagb@cluster0.mnvwtnr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
+// register as user
 app.post('/register', async (req, res) => {
     const {username, password} = req.body;
     try {
@@ -35,6 +36,7 @@ app.post('/register', async (req, res) => {
     }
 })
 
+// login and sign jwt
 app.post('/login', async (req,res) => {
     const {username,password} = req.body;
     const userDoc = await UserModel.findOne({username});
@@ -53,6 +55,7 @@ app.post('/login', async (req,res) => {
     }
   });
 
+  // get user info using jwt
   app.get('/profile', (req, res) => {
     const {token} = req.cookies; 
     jwt.verify(token, secret, {}, (err, info) => {
@@ -61,10 +64,13 @@ app.post('/login', async (req,res) => {
     });
   })
 
+  // logout of app
   app.post('/logout', (req, res) => {
     res.cookie('token', '').json('ok');
   })
 
+
+ // create post
   app.post('/post',uploadMiddleware.single('file'), async (req,res) => {
     const {originalname, path} = req.file;
     const parts = originalname.split('.');
@@ -88,17 +94,49 @@ app.post('/login', async (req,res) => {
     });
   });
 
+  // get all posts
   app.get('/post', async (req, res) => {
     const posts = await PostModel.find().populate('author', ['username']).sort({createdAt: - 1})
     res.json(posts); 
   }); 
 
+  // get single post
   app.get('/post/:id', async (req, res) =>{
     const {id} = req.params; 
     const postDoc = await PostModel.findById(id).populate('author', ['username']); 
     res.json(postDoc); 
   })
 
+  // update post
+  app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
+    let newPath = null;
+    if (req.file) {
+        const {originalname, path} = req.file;
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1]; 
+        newPath = path +'.'+ext;
+        fs.renameSync(path, newPath )
+    }
+
+    const {token} = req.cookies; 
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const {id, title, summary, content} = req.body; 
+        const postDoc = await PostModel.findById(id); 
+        const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id); 
+        
+        if (!isAuthor) return res.status(400).json('you are not the author'); 
+
+        await postDoc.updateOne({
+            title, 
+            summary, 
+            content,
+            cover: newPath ? newPath : postDoc.cover,
+        })
+        res.json(postDoc); 
+    });
+
+  })
 
 app.listen(4000);
 
