9fe1a733d8d9a802d8fbbc9ae71e6186f46baff0
Tau <tausifsolaiman@gmail.com>
2024-08-02 12:19:07 -0400

    cors

--- api/index.js
+++ api/index.js
@@ -16,7 +16,14 @@ require('dotenv').config()
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({ origin: "*"})); 
+const corsOptions = {
+    origin: (origin, callback) => {
+      callback(null, true);
+    },
+    credentials: true
+  };
+  
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser()); 
 app.use('/uploads', express.static(__dirname + '/uploads'));
